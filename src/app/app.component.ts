44bf37702d53b4f582642be4e6da6bbe8919c51d
iTzMiNOS <amirerf1998@gmail.com>
2023-06-24 12:05:47 +0300

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/styles.css 	new file:   tailwind.config.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,90 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCalc';
+
+  calValue: number = 0;
+
+  funcT: any = '\u200B';
+  calNum: string = 'noValue';
+
+  FirstOperand: number = NaN;
+  secondOperand: number = NaN;
+
+  onClickValue(val:string, type: any){
+    if(type == 'num'){
+      this.onNumberClick(val);
+    }else if(type == 'func'){
+      this.onFuncClick(val);
+    }
+
+  }
+  onNumberClick(val:string){
+     if(this.calNum != 'noValue'){
+      this.calNum = this.calNum + val;
+     }else{
+      this.calNum = val;
+     }
+
+     this.calValue = (parseFloat(this.calNum));
+     //this.calValue.toFixed(0);
+  }
+  onFuncClick(val:string){
+    if(val == 'C'){
+      this.clearFunc();
+    }else if(this.funcT == '\u200B' && val != '='){
+      this.FirstOperand = this.calValue;
+      this.calValue = 0;
+      this.calNum = 'noValue';
+      this.funcT = val;
+    }else if(this.funcT != '\u200B'){
+      this.secondOperand = this.calValue;
+      this.valueCalculate(val);
+    }
+  }
+  valueCalculate(val:string){
+    if(this.funcT == '+'){
+      const Result = this.FirstOperand + this.secondOperand;
+      this.resultAssignValues(Result,val);
+      if(val == '=') this.onEqualPress();
+    }else if(this.funcT == '-'){
+      const Result = this.FirstOperand - this.secondOperand;
+      this.resultAssignValues(Result,val);
+      if(val == '=') this.onEqualPress();
+    }else if(this.funcT == '/'){
+      const Result = this.FirstOperand / this.secondOperand;
+      this.resultAssignValues(Result,val);
+      if(val == '=') this.onEqualPress();
+    }else if(this.funcT == 'x'){
+      const Result = this.FirstOperand * this.secondOperand;
+      this.resultAssignValues(Result,val);
+      if(val == '=') this.onEqualPress();
+    }else if(this.funcT == '%'){
+      this.secondOperand = this.secondOperand/100;
+      const Result = this.FirstOperand * this.secondOperand/100;
+      this.resultAssignValues(Result,val);
+      if(val == '=') this.onEqualPress();
+    }
+  }
+  resultAssignValues(Result: number, val: string){
+    this.FirstOperand = Result;
+    this.secondOperand = NaN;
+    this.calNum = 'noValue';
+    this.funcT = val;
+    this.calValue = Result;
+  }
+  onEqualPress(){
+    this.FirstOperand = NaN;
+    this.secondOperand = NaN;
+    this.funcT = '\u200B';
+    this.calNum = 'noValue';
+  }
+  clearFunc(){
+    this.FirstOperand = NaN;
+    this.secondOperand = NaN;
+    this.calValue = 0;
+    this.calNum = 'noValue';
+    this.funcT = '\u200B';
+    
+  }
+
 }
